580d8e9d7f996b51f6393f607f87f9759e038410
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-04-10 21:55:18 -0300

    first commit

---
+++ src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/SistemaRaizesDoNordesteApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielbcunha.sistemaraizesdonordeste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaRaizesDoNordesteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaRaizesDoNordesteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/gabrielbcunha/sistemaraizesdonordeste/SistemaRaizesDoNordesteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielbcunha.sistemaraizesdonordeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaRaizesDoNordesteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
