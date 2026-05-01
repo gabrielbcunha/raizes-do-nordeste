f7a0e4f1bb44d2629e47647824f400b7775e43f5
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-05-01 16:29:35 -0300

    feat(repository): cria método para busca de userName

--- src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/repository/UsuarioRepository.java
+++ src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/repository/UsuarioRepository.java
@@ -3,5 +3,10 @@ package br.com.gabrielbcunha.sistemaraizesdonordeste.repository;
 import br.com.gabrielbcunha.sistemaraizesdonordeste.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByUserName(String userName);
+
 }
