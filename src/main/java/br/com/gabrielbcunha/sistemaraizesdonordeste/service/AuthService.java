c2fa64382c222062789276abf9775cfbbe24dd54
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-05-04 00:22:57 -0300

    feat(Service): criar Service para Auth com Login e Cadastro de Clientes

---
+++ src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/service/AuthService.java
@@ -0,0 +1,65 @@
+package br.com.gabrielbcunha.sistemaraizesdonordeste.service;
+
+import br.com.gabrielbcunha.sistemaraizesdonordeste.dto.auth.CadastroClienteRequest;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.dto.auth.CadastroClienteResponse;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.dto.auth.LoginRequest;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.dto.auth.TokenResponse;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.mapper.ClienteMapper;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.model.entity.Cliente;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.model.entity.Usuario;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.model.enums.Perfil;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.repository.ClienteRepository;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.security.JwtService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthService {
+
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+    private final ClienteRepository clienteRepository;
+    private final ClienteMapper clienteMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthService(JwtService jwtService, AuthenticationManager authenticationManager, ClienteRepository clienteRepository, ClienteMapper clienteMapper, PasswordEncoder passwordEncoder) {
+        this.jwtService = jwtService;
+        this.authenticationManager = authenticationManager;
+        this.clienteRepository = clienteRepository;
+        this.clienteMapper = clienteMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public CadastroClienteResponse cadastrarCliente(CadastroClienteRequest request){
+
+        Usuario novoUsuario = new Usuario();
+        novoUsuario.setUserName(request.getEmail());
+        String senha = this.passwordEncoder.encode(request.getSenha());
+        novoUsuario.setSenha(senha);
+        novoUsuario.setPerfil(Perfil.ROLE_CLIENTE);
+
+        String numeroFidelidade = java.util.UUID.randomUUID().toString();
+
+        Cliente novoCliente = clienteMapper.toEntity(request);
+        novoCliente.setUsuario(novoUsuario);
+        novoCliente.setNumCadastroFidelidade(numeroFidelidade);
+        novoCliente.setQuantPontosFidelidade(0);
+
+        Cliente cliente = clienteRepository.save(novoCliente);
+        return clienteMapper.toDto(cliente);
+    }
+
+    public TokenResponse fazerLogin(LoginRequest request){
+        UsernamePasswordAuthenticationToken login = new UsernamePasswordAuthenticationToken(request.getUserName(), request.getSenha());
+        Authentication autenticacao = authenticationManager.authenticate(login);
+        Usuario usuario = (Usuario) autenticacao.getPrincipal();
+        String Token = jwtService.geradorToken(usuario);
+        TokenResponse tokenResponse = new TokenResponse(Token);
+        return tokenResponse;
+    }
+
+
+}
