0454742a02de6ddbb9bbb0931729eda6236f355f
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-05-04 21:21:38 -0300

    feat(enums): criar novos ROLES

--- src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/model/enums/Perfil.java
+++ src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/model/enums/Perfil.java
@@ -3,6 +3,9 @@ package br.com.gabrielbcunha.sistemaraizesdonordeste.model.enums;
 public enum Perfil {
     ROLE_CLIENTE,
     ROLE_ATENDENTE,
+    ROLE_COZINHEIRO,
+    ROLE_ADMINISTRATIVO,
+    ROLE_RECURSOS_HUMANOS,
     ROLE_GERENTE,
     ROLE_ADMIN
 }
