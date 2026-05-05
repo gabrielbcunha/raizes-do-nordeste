ec9d888c92bf74d462f3f46576b3a393d45ea37a
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-05-04 21:50:02 -0300

    refactor(funcionario): remover o pedido de Cargo do DTO Request

--- src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/dto/funcionario/FuncionarioCreateRequest.java
+++ src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/dto/funcionario/FuncionarioCreateRequest.java
@@ -1,6 +1,5 @@
 package br.com.gabrielbcunha.sistemaraizesdonordeste.dto.funcionario;
 
-import br.com.gabrielbcunha.sistemaraizesdonordeste.model.enums.Cargo;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -13,9 +12,6 @@ public class FuncionarioCreateRequest {
 
     @NotNull(message="O ID da únidade deve existir")
     private Long idUnidade;
-
-    @NotNull(message="O cargo do funcionário deve existir")
-    private Cargo cargo;
 
     @NotBlank(message="O nome do funcionário deve existir")
     private String nome;
