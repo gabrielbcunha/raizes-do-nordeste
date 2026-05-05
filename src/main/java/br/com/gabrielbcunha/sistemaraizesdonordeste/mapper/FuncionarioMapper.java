add4d83a584089a2928149773bd24ddb7485bd15
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-05-04 21:24:03 -0300

    feat(mapper): cria Mapper para Funcionario

---
+++ src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/mapper/FuncionarioMapper.java
@@ -0,0 +1,15 @@
+package br.com.gabrielbcunha.sistemaraizesdonordeste.mapper;
+
+import br.com.gabrielbcunha.sistemaraizesdonordeste.dto.funcionario.FuncionarioCreateRequest;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.dto.funcionario.FuncionarioResponse;
+import br.com.gabrielbcunha.sistemaraizesdonordeste.model.entity.Funcionario;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel="spring")
+public interface FuncionarioMapper {
+
+    Funcionario toEntity(FuncionarioCreateRequest funcionarioCreateRequest);
+
+    FuncionarioResponse toDto(Funcionario funcionario);
+
+}
