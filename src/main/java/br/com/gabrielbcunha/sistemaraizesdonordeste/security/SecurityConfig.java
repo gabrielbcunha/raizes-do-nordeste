aed8146034bf62c943e4ee39c9fa6a8dfeb9359d
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-05-04 22:03:11 -0300

    feat(security): restringir requisição POST em /funcionarios/atendentes

--- src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/security/SecurityConfig.java
+++ src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/security/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
 
                         .requestMatchers(HttpMethod.POST, "/auth/login", "/auth/cadastrar").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/funcionarios/atendentes").hasAnyRole("ADMIN", "GERENTE")
                         //...
                         .anyRequest().authenticated()
                 )
