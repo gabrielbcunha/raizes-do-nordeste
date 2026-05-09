f940af7fd691e2378734820829e97bc1ca84b344
Gabriel B Cunha <gabriel_sampa@outlook.com>
2026-05-09 17:05:49 -0300

    feat(exception): criar exceção para estoque insuficiente

---
+++ src/main/java/br/com/gabrielbcunha/sistemaraizesdonordeste/exception/EstoqueInsuficienteException.java
@@ -0,0 +1,7 @@
+package br.com.gabrielbcunha.sistemaraizesdonordeste.exception;
+
+public class EstoqueInsuficienteException extends RuntimeException {
+    public EstoqueInsuficienteException(String message) {
+        super(message);
+    }
+}
